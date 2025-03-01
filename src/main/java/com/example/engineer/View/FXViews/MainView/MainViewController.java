c6822065a7c1a5d19103e710fdafa0441cb0ce08
s22787 <101191151+s22787@users.noreply.github.com>
2025-03-01 22:32:26 +0100

    fix file validation issue

--- src/main/java/com/example/engineer/View/FXViews/MainView/MainViewController.java
+++ src/main/java/com/example/engineer/View/FXViews/MainView/MainViewController.java
@@ -317,21 +317,21 @@ public class MainViewController implements LanguageChangeListener, UpdateTableLi
     }
 
     private boolean isValidFile(File file) throws Exception{
-        if(file.getName().endsWith(".gif"))
+        if(file.getName().endsWith("gif"))
             return true;
 
         if(new Tika().detect(file).equals("video/"))
             return true;
 
         var extensions = new String[]{
-                "*.gif","*.webm","*.mkv","*.flv","*.vob",
-                "*.ogv","*.ogg","*.rrc","*.gifv","*.mng",
-                "*.mov","*.avi","*.qt","*.wmv","*.yuv",
-                "*.rm","*.asf","*.amv","*.mp4","*.m4p",
-                "*.m4v","*.mpg","*.mp2","*.mpeg","*.mpe",
-                "*.mpv","*.m4v","*.svi","*.3gp","*.3g2",
-                "*.mxf","*.roq","*.nsv","*.flv","*.f4v",
-                "*.f4p","*.f4a","*.f4b","*.mod"
+                "gif","webm","mkv","flv","vob",
+                "ogv","ogg","rrc","gifv","mng",
+                "mov","avi","qt","wmv","yuv",
+                "rm","asf","amv","mp4","m4p",
+                "m4v","mpg","mp2","mpeg","mpe",
+                "mpv","m4v","svi","3gp","3g2",
+                "mxf","roq","nsv","flv","f4v",
+                "f4p","f4a","f4b","mod"
         };
 
         for(String extension : extensions)
